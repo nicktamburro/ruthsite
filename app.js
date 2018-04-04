9cef915f88628b975196a62dff9381ffc8783136
nicktamburro <nicholastamburro@gmail.com>
2018-04-03 21:41:20 -0700

    working on mobile, small screens

--- app.js
+++ app.js
@@ -3,34 +3,26 @@ $(document).ready(function(){
 
 
 if ( $(window).width() > 739) {     
-  $("#menu").hide(); 
-  $("#close").hide();
-  $("#links").show();
+  	$("#menu").hide(); 
+  	$("#close").hide();
+  	$("#links").show();
 } 
 else {
-  $("#links").hide();
-  $("#close").hide();
-
-$("#menu").click(function(event){
-	event.preventDefault();
-		$("#menu").hide();
-		$("#close").show();
-		$("#links").show();
-	});
-
-$("#close").click(function(event){
-	event.preventDefault();
-	$("#close").hide();
-	$("#menu").show();
 	$("#links").hide();
-})
+  	$("#close").hide();
 
+		$("#menu").click(function(event){
+			event.preventDefault();
+			$("#menu").hide();
+			$("#close").show();
+			$("#links").show();
+		});
 
-
-
-}
-
-
-
-
+		$("#close").click(function(event){
+			event.preventDefault();
+			$("#close").hide();
+			$("#menu").show();
+			$("#links").hide();
+		})
+	}
 });
