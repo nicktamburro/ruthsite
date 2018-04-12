c60c92c3fc9ea8089f6e3263003d3cae510028e9
nicktamburro <nicholastamburro@gmail.com>
2018-04-11 21:40:30 -0700

    new book blurb

---
+++ media/InchesInMile.java
@@ -0,0 +1,16 @@
+class InchesInMile{
+	public static void main(String args[]){
+
+		long ci;
+		long im;
+
+		im = 5280 * 12;
+
+		ci = im * im * im;
+
+		System.out.println("There are " + ci + " cubic inches in a cubic mile!");
+
+
+	}
+
+}
